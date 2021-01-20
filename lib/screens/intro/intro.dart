30319a5d078d5ea65144fce9a18fccd8b34d3919
hoangthienvn97 <hoangthien6197@gmail.com>
2021-01-20 13:35:56 +0700

    add .

--- lib/screens/intro/intro.dart
+++ lib/screens/intro/intro.dart
@@ -21,32 +21,27 @@ class _IntroBlockState extends State<IntroBlock> {
         children: [
           Expanded(
             flex: 1,
-            child: Column(
-              children: [
-                Expanded(
-                  flex: 3,
-                  child: Padding(
-                      padding: const EdgeInsets.only(top: 84.0),
-                      child: Text(
-                        Localized.get.introTitle,
-                        style: TextStyles.Headline2.apply(
-                            color: AppColors.PRIMARY),
-                      )),
-                ),
-                Expanded(
-                  flex: 1,
-                  child: Padding(
+            child: Container(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    Localized.get.introTitle,
+                    style: TextStyles.Headline2.apply(color: AppColors.PRIMARY),
+                  ),
+                  Padding(
                       padding: const EdgeInsets.only(top: 8.0),
                       child: Text(
                         Localized.get.introText,
                         style: TextStyles.Body2,
                       )),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
           Expanded(
-            flex: 3,
+            flex: 4,
             child: Column(
               children: [
                 Padding(
@@ -57,64 +52,70 @@ class _IntroBlockState extends State<IntroBlock> {
                 ),
                 Padding(
                   padding: const EdgeInsets.only(top: 40.0),
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 30.0),
-                    child: Align(
-                      alignment: Alignment.centerLeft,
-                      child: Column(
-                        children: [
-                          Row(
-                            children: [
-                              IntroSetting("1.", Localized.get.introSetupStep11,
-                                  Localized.get.introSetupStep12),
-                              Padding(
-                                padding: const EdgeInsets.only(left: 5),
-                                child: Image.asset(Assets.ICON_SETTING_INTRO),
+                  child: Container(
+                    alignment: Alignment.center,
+                    child: Container(
+                      width: 400,
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 25.0),
+                        child: Column(
+                          children: [
+                            Row(
+                              children: [
+                                IntroSetting(
+                                    "1.",
+                                    Localized.get.introSetupStep11,
+                                    Localized.get.introSetupStep12),
+                                Padding(
+                                  padding: const EdgeInsets.only(left: 5),
+                                  child: Image.asset(Assets.ICON_SETTING_INTRO),
+                                ),
+                              ],
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(top: 12.0),
+                              child: Row(
+                                children: [
+                                  IntroSetting(
+                                      "2.",
+                                      Localized.get.introSetupStep21,
+                                      Localized.get.introSetupStep22),
+                                  Padding(
+                                    padding: const EdgeInsets.only(left: 5),
+                                    child: Image.asset(Assets.ICON_PHONE_INTRO),
+                                  ),
+                                ],
                               ),
-                            ],
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(top: 12.0),
-                            child: Row(
-                              children: [
-                                IntroSetting(
-                                    "2.",
-                                    Localized.get.introSetupStep21,
-                                    Localized.get.introSetupStep22),
-                                Padding(
-                                  padding: const EdgeInsets.only(left: 5),
-                                  child: Image.asset(Assets.ICON_PHONE_INTRO),
-                                ),
-                              ],
                             ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(top: 12.0),
-                            child: Row(
-                              children: [
-                                IntroSetting(
-                                    "3.",
-                                    Localized.get.introSetupStep31,
-                                    Localized.get.introSetupStep32),
-                              ],
+                            Padding(
+                              padding: const EdgeInsets.only(top: 12.0),
+                              child: Row(
+                                children: [
+                                  IntroSetting(
+                                      "3.",
+                                      Localized.get.introSetupStep31,
+                                      Localized.get.introSetupStep32),
+                                ],
+                              ),
                             ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(top: 12.0),
-                            child: Row(
-                              children: [
-                                IntroSetting(
-                                    "4.",
-                                    Localized.get.introSetupStep41,
-                                    Localized.get.introSetupStep42),
-                                Padding(
-                                  padding: const EdgeInsets.only(left: 5),
-                                  child: Image.asset(Assets.ICON_SWITCH_INTRO),
-                                ),
-                              ],
+                            Padding(
+                              padding: const EdgeInsets.only(top: 12.0),
+                              child: Row(
+                                children: [
+                                  IntroSetting(
+                                      "4.",
+                                      Localized.get.introSetupStep41,
+                                      Localized.get.introSetupStep42),
+                                  Padding(
+                                    padding: const EdgeInsets.only(left: 5),
+                                    child:
+                                        Image.asset(Assets.ICON_SWITCH_INTRO),
+                                  ),
+                                ],
+                              ),
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
                     ),
                   ),
@@ -123,7 +124,7 @@ class _IntroBlockState extends State<IntroBlock> {
             ),
           ),
           Padding(
-            padding: const EdgeInsets.only(bottom : 16.0),
+            padding: const EdgeInsets.only(bottom: 10.0),
             child: ButtonSecondary(
               onTap: () => navigatorPush(context, Login()),
               background: AppColors.PRIMARY,
